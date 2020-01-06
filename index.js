433b99e650b886a150010d06a04c08b60a221e6a
Khoa Luu <moonjava@gmail.com>
2020-01-06 16:38:54 +0700

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import TextInputKeyboardMangerIOS from './src/TextInputKeyboardMangerIOS';
+import CustomKeyboardView from './src/CustomKeyboardView';
+import KeyboardAccessoryView from './src/KeyboardAccessoryView';
+import KeyboardRegistry from './src/KeyboardsRegistry';
+import KeyboardUtils from './src/utils/KeyboardUtils';
 
-import { NativeModules } from 'react-native';
-
-const { RNInteractiveKeyboard } = NativeModules;
-
-export default RNInteractiveKeyboard;
+export {TextInputKeyboardMangerIOS, CustomKeyboardView, KeyboardRegistry, KeyboardAccessoryView, KeyboardUtils};
